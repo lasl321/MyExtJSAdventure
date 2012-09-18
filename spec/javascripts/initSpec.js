3800811781b67921a8a9e3a04ac71df222f71157
Luis A. Sotomayor <luis.sotomayor@gmail.com>
2012-09-18 00:14:51 -0700

    Added jasmine to project

---
+++ spec/javascripts/initSpec.js
@@ -0,0 +1,8 @@
+var vehicle = Ext.create('Cookbook.Vehicle');
+
+describe('Cookbook.Vehicle', function() {
+    it('should create an alert', function() {
+        vehicle.getDetails();
+    });
+});
+
